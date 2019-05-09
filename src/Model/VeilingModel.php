7a501d241e7b0472a2c26f75d0ea954d96657eac
Joey Boerwinkel <joey.gameslabs@gmail.com>
2019-05-09 10:56:39 +0200

    fix other models

--- src/Model/VeilingModel.php
+++ src/Model/VeilingModel.php
@@ -7,4 +7,21 @@ class VeilingModel extends Model {
     public $timer;
     public $price;
     public $image;
+
+    /**
+     * @return string the field used as primary key for the entity
+     */
+    public function getIdentifier()
+    {
+        return ""; //TODO: return correct field
+    }
+
+    /**
+     *
+     * @return string the start of the API path, for example rubrieken
+     */
+    protected function getBaseApiPath(): string
+    {
+        return "veilingen";
+    }
 }
