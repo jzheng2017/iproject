404930fe0b98ef757de60d187f4cebb6b8c3d346
Joey Boerwinkel <joey.gameslabs@gmail.com>
2019-05-11 14:31:45 +0200

    create veilingdetail

--- src/Controller/VeilingController.php
+++ src/Controller/VeilingController.php
@@ -1,20 +1,20 @@
 <?php
 namespace EenmaalAndermaal\Controller;
 
+use EenmaalAndermaal\Model\VeilingModel;
 use EenmaalAndermaal\Request\Request;
 use EenmaalAndermaal\Request\RequestMethod;
 use EenmaalAndermaal\Route\Route;
 use EenmaalAndermaal\Route\Router;
-use EenmaalAndermaal\View\View;
+use EenmaalAndermaal\View\VeilingDetailView;
 
 class VeilingController implements Controller {
 
     public function registerRoutes(Router &$router)
     {
         $router->addRoute(new Route("veiling/{id}", RequestMethod::GET(), function (Request $request) {
-            $view = new View("veilingen/veiling_detail");
-            $view->homepage = false;
-            return $view->render();
+            $v = new VeilingModel();
+            return (new VeilingDetailView($v))->render();
         }));
     }
 }
