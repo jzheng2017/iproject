a64512ec154292a912d882ce6ac5a1e94d734a36
Steven Piedra <stevenpiedra-@live.nl>
2019-05-11 18:12:34 +0200

    Changes made to veiling_detail page

--- scripts/veilingen/veiling_detail.js
+++ scripts/veilingen/veiling_detail.js
@@ -1,4 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems, {});
+    var instance = M.Carousel.init({
+        fullWidth: true
+    });
 });
+
+$('.carousel').carousel({
+    padding: 200
+});
+
+$(document).ready(function(){
+    $('ul.tabs').tabs();
+});
+
+$(document).ready(function() {
+    M.updateTextFields();
+});
+
+autoplay();
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 4500);
+}
+
+
+
