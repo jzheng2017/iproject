1934222863f303e17eccd36bf9f5a9a2df1bb551
Steven Piedra <stevenpiedra-@live.nl>
2019-05-17 16:40:57 +0200

    Added Registratieview

---
+++ src/Controller/GebruikersController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Steven
+ * Date: 17/05/2019
+ * Time: 13:24
+ */
+
+namespace EenmaalAndermaal\Controller;
+
+
+class RegistratieController
+{
+
+}
---
+++ src/View/RegistratieView.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Steven
+ * Date: 17/05/2019
+ * Time: 13:32
+ */
+
+namespace EenmaalAndermaal\View;
+
+
+class GebruikersView
+{
+
+}
