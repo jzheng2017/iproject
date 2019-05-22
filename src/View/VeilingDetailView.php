c4b64257604aa4daede64c45fa2d509745bec8c9
jzheng2017 <jianjian-r@hotmail.com>
2019-05-22 10:07:38 +0200

    add related veilingen to veiling detail page

--- src/View/VeilingDetailView.php
+++ src/View/VeilingDetailView.php
@@ -3,18 +3,21 @@
 namespace EenmaalAndermaal\View;
 
 use EenmaalAndermaal\Model\VeilingModel;
+use EenmaalAndermaal\Model\VeilingModelCollection;
 
 class VeilingDetailView extends View
 {
 
     public $veiling;
-
+    public $collection;
     public function __construct(VeilingModel $veiling)
     {
         parent::__construct("veilingen/veiling_detail");
         $this->veiling = $veiling;
         $this->homepage = false;
+        $this->collection = new VeilingModelCollection();
         $this->addStyle("style/veilingen");
         $this->addScript("scripts/veilingen/veiling_detail");
     }
+
 }
