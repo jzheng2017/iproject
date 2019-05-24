7f4472b916f740dd3d5b9b041ba6b657812dada1
jzheng2017 <jianjian-r@hotmail.com>
2019-05-24 11:10:10 +0200

    add form post handling/validation

--- src/Controller/GebruikersController.php
+++ src/Controller/GebruikersController.php
@@ -8,6 +8,7 @@ use EenmaalAndermaal\Request\Request;
 use EenmaalAndermaal\Request\RequestMethod;
 use EenmaalAndermaal\Route\Route;
 use EenmaalAndermaal\Route\Router;
+use EenmaalAndermaal\Util\Debug;
 use EenmaalAndermaal\View\RegistratieView;
 
 class GebruikersController implements Controller
@@ -16,6 +17,10 @@ class GebruikersController implements Controller
     public function registerRoutes(Router &$router)
     {
         $router->addRoute(new Route("registreren", RequestMethod::GET(), function (Request $request) {
+            return (new RegistratieView())->render();
+        }));
+
+        $router->addRoute(new Route("registreren", RequestMethod::POST(), function (Request $request) {
             return (new RegistratieView())->render();
         }));
     }
