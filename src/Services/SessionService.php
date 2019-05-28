ba70ec4977745e82508d6ae5b3b35c0d4a0fd999
Joey Boerwinkel <joey.gameslabs@gmail.com>
2019-05-28 21:03:18 +0200

    hopeful session fix

--- src/Services/SessionService.php
+++ src/Services/SessionService.php
@@ -18,7 +18,7 @@ class SessionService
 
     function __construct()
     {
-        self::sessionStart("EenmaalAndermaalSession");
+        self::sessionStart();
     }
 
     /**
@@ -86,38 +86,9 @@ class SessionService
         session_destroy();
     }
 
-    static function sessionStart($name, $limit = 0, $path = '/', $domain = null, $secure = null)
+    static function sessionStart()
     {
-        // Set the cookie name
-        session_name($name . '_Session');
-
-        // Set SSL level
-        $https = isset($secure) ? $secure : isset($_SERVER['HTTPS']);
-
-        // Set session cookie options
-        session_set_cookie_params($limit, $path, $domain, $https, true);
         session_start();
-
-        // Make sure the session hasn't expired, and destroy it if it has
-        if (self::validateSession()) {
-            // Check to see if the session is new or a hijacking attempt
-            if (!self::preventHijacking()) {
-                // Reset session data and regenerate id
-                $_SESSION = array();
-                if (isset($_SERVER['REMOTE_ADDR'])) {
-                    $_SESSION['IPaddress'] = $_SERVER['REMOTE_ADDR'];
-                    $_SESSION['userAgent'] = $_SERVER['HTTP_USER_AGENT'];
-                    self::regenerateSession();
-                }
-                // Give a 5% chance of the session id changing on any request
-            } elseif (rand(1, 100) <= 5) {
-                self::regenerateSession();
-            }
-        } else {
-            $_SESSION = array();
-            session_destroy();
-            session_start();
-        }
     }
 
     static protected function validateSession()
